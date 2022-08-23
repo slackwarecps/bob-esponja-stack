b83520bc1840147c13e69c5e06c7e87a9240acc1
fabioalvaropereira <fabio.pereira@icarros.com.br>
2022-08-23 08:57:36 -0300

    upload de teste de stack

--- template-patrick1/templates/src/app/app-routing.module.ts
+++ template-patrick1/templates/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'index', component: AppComponent },
+  { path: '', component: AppComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
